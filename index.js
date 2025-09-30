e4cf39d2e36fadb35726694b919be2f99708c38b
thiagojordao98 <thiagojordao98@gmail.com>
2025-09-30 17:42:35 -0300

    Remove hardcoded Chrome executable path in Puppeteer launch options

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get("/search", async (req, res) => {
   try {
     const browser = await puppeteer.launch({
       headless: true,
-      executablePath: "/usr/bin/google-chrome-stable",
+      // executablePath: "/usr/bin/google-chrome-stable",
       args: ["--no-sandbox", "--disable-setuid-sandbox", "--lang=pt-BR"],
     });
 
